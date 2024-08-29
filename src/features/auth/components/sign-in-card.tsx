611ff7badc43c5e2984df3822b0ee636ba06d10e
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-29 19:58:55 +0530

    feat: Add Password as an authentication provider for sign-in

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
 import { useAuthActions } from '@convex-dev/auth/react';
+import { TriangleAlert } from 'lucide-react';
 import React, { useState } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
@@ -16,7 +17,16 @@ const SignInCard = ({ setState }: SignInCardProps) => {
   const { signIn } = useAuthActions();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const [pending, setPending] = useState(false);
+
+  const onPasswordSignIn = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setPending(true);
+    signIn('password', { email, password, flow: 'signIn' })
+      .catch(() => setError('Invalid email or password'))
+      .finally(() => setPending(false));
+  };
 
   const onProviderSignIn = (value: 'github' | 'google') => {
     setPending(true);
@@ -29,8 +39,16 @@ const SignInCard = ({ setState }: SignInCardProps) => {
         <CardTitle>Login to continue</CardTitle>
         <CardDescription>Use your email or other service to continue</CardDescription>
       </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex  items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4" />
+          {error}
+        </div>
+      )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form
+          onSubmit={onPasswordSignIn}
+          className="space-y-2.5">
           <Input
             type="email"
             value={email}
