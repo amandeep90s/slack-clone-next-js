a000a8aed8601c4cde391d768abd0ac52ba72e03
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-29 14:21:21 +0530

    feat: Setup convex and add convex tasks API endpoint and sample data

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -9,7 +9,7 @@ export const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>('signIn');
 
   return (
-    <div className="h-full flex items-center justify-center bg-[#5c3b58]">
+    <div className="h-full flex items-center justify-center bg-[#703259]">
       <div className="md:h-auto md:w-[420px]">
         {state === 'signIn' ? <SignInCard setState={setState} /> : <SignUpCard setState={setState} />}
       </div>
