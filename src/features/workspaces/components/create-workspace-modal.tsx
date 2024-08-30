c773d2afcd5894109e2bcec5d3552d56c3639e9b
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-30 13:24:58 +0530

    chore: Add create workspace mutation and API hook

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -3,14 +3,31 @@
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import { useCreateWorkspace } from '../api/use-create-workspace';
 import { useCreateWorkspaceModal } from '../store/use-create-workspace-modal';
 
 const CreateWorkspaceModal = () => {
   const [open, setOpen] = useCreateWorkspaceModal();
+  const [name, setName] = useState('');
+
+  const { mutate, isPending } = useCreateWorkspace();
 
   const handleClose = () => {
     setOpen(false);
     // TODO: Clean form
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    mutate(
+      { name },
+      {
+        onSuccess(data) {
+          console.log(data);
+        }
+      }
+    );
   };
 
   return (
@@ -21,20 +38,26 @@ const CreateWorkspaceModal = () => {
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
         </DialogHeader>
-        <form className="space-y-4">
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-4">
           <Input
-            value=""
-            disabled={false}
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+            disabled={isPending}
             required
             autoFocus
             minLength={3}
             placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
           />
+          <div className="flex justify-end">
+            <Button
+              type="submit"
+              disabled={isPending}>
+              Create
+            </Button>
+          </div>
         </form>
-
-        <div className="flex justify-end">
-          <Button disabled={false}>Create</Button>
-        </div>
       </DialogContent>
     </Dialog>
   );
