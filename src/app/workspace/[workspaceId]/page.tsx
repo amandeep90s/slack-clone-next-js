ffc0ae4127e1f7e9398b7b8c6650b8e9c6d694a3
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-30 16:20:02 +0530

    feat: Add useGetWorkspace hook for fetching workspace data

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 import { useWorkspaceId } from '@/hooks/use-workspace-id';
 import React from 'react';
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
 
   return <div>{workspaceId}</div>;
 };
