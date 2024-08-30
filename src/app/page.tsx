66885595e37aa834d3596b585e5a2549d46ffa0b
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-30 09:21:58 +0530

    chore: Add workspaces module and API endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 'use client';
 
 import UserButton from '@/features/auth/components/user-button';
+import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces';
+import { useEffect, useMemo } from 'react';
 
 export default function Home() {
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => {
+    return data?.[0]?._id;
+  }, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      console.log('Redirect to workspace');
+    } else {
+      console.log('Open creation modal');
+    }
+  }, [workspaceId, isLoading]);
+
   return <UserButton />;
 }
